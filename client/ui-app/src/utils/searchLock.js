59463ff354e07301de9a120d1a9fabde595763f6
root <root@server.massclick.in>
2026-04-08 17:41:32 +0530

    Add all important changes moved from master to dev

---
+++ client/ui-app/src/utils/searchLock.js
@@ -0,0 +1,15 @@
+
+let lastSearchKey = null;
+let lastTime = 0;
+
+export const shouldSendSearch = (key) => {
+  const now = Date.now();
+
+  if (lastSearchKey === key && now - lastTime < 2000) {
+    return false;
+  }
+
+  lastSearchKey = key;
+  lastTime = now;
+  return true;
+};
