ab8c8f46daf5ca149eb865463fcdced805fa521c
Massclick-App <zodiac29022000@gmail.com>
2026-04-09 17:46:55 +0530

    just testing

--- server/helper/firebaseInit.js
+++ server/helper/firebaseInit.js
@@ -4,7 +4,6 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const serviceAccount = require("../key.json");
 
-// Initialize Firebase Admin if it hasn't been initialized yet
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
