f988d92aad295f89192c388bf6c67de1008bed02
ABISHEK M K <abishek.mk.01@gmail.com>
2026-04-22 12:55:34 +0530

    Update favoriteSchema.js

--- server/schema/favorite/favoriteSchema.js
+++ server/schema/favorite/favoriteSchema.js
@@ -3,12 +3,12 @@ import mongoose from "mongoose";
 const favoriteSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+    ref: "user",
     required: true,
   },
   businessId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "BusinessList",
+    ref: "businesslist",
     required: true,
   },
   createdAt: {
