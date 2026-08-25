d67f3f2347e5283a2c8d149bd7d17ec04c6b51d1
ABISHEK M K <abishek.mk.01@gmail.com>
2026-08-25 18:31:49 +0530

    Load Firebase credentials from mounted path

--- server/helper/firebaseInit.js
+++ server/helper/firebaseInit.js
@@ -1,8 +1,26 @@
 import admin from "firebase-admin";
 import { createRequire } from "module";
+import fs from "fs";
 
 const require = createRequire(import.meta.url);
-const serviceAccount = require("../key.json");
+
+const loadServiceAccount = () => {
+  const credentialPaths = [
+    process.env.FIREBASE_SERVICE_ACCOUNT_PATH,
+    process.env.GOOGLE_APPLICATION_CREDENTIALS,
+    "/home/admin/credentials/massclick-dc8f6-4ac3adcfe0be.json",
+  ].filter(Boolean);
+
+  for (const credentialPath of credentialPaths) {
+    if (fs.existsSync(credentialPath)) {
+      return JSON.parse(fs.readFileSync(credentialPath, "utf8"));
+    }
+  }
+
+  return require("../key.json");
+};
+
+const serviceAccount = loadServiceAccount();
 
 if (!admin.apps.length) {
   admin.initializeApp({
